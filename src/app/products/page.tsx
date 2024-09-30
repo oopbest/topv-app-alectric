6771c7b32ffabcf7e4095cf2cb0fdae55ece5923
Setthawut Pimthong <oop_best@yahoo.com>
2024-09-30 12:44:23 +0700

    update product rating

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // -- Solid icons
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { getProducts } from "./get-products";
 import Link from "next/link";
 import { formatThaiBaht } from "@/utils/format-currency";
+import ProductRating from "@/components/products/product-rating";
 
 // interface Product {
 //   id: number;
@@ -31,42 +31,38 @@ export default async function ProductsPage() {
     <section className="flex mx-auto bg-theme-container">
       <div className="container mx-auto p-4">
         <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="bg-white rounded-2xl flex items-center justify-center"
-            >
-              <div className="flex flex-col text-center gap-3">
-                <Image
-                  width={500}
-                  height={500}
-                  src="https://picsum.photos/500/500"
-                  alt={product.name}
-                  className="object-contain"
-                />
-                <p className="px-5 line-clamp-2">{product.name}</p>
-                <p className="font-bold">{formatThaiBaht(product.price)}</p>
-                <div>
-                  <FontAwesomeIcon icon={faStar} className="text-yellow-300" />{" "}
-                  <FontAwesomeIcon icon={faStar} className="text-yellow-300" />{" "}
-                  <FontAwesomeIcon icon={faStar} className="text-yellow-300" />{" "}
-                  <FontAwesomeIcon icon={faStar} className="text-yellow-300" />{" "}
-                  <FontAwesomeIcon icon={faStar} className="text-yellow-300" />{" "}
-                  <span>{product.rating}</span>
+          {products.map((product) => {
+            const productRating = product.rating;
+            return (
+              <div
+                key={product.id}
+                className="bg-white rounded-2xl flex items-center justify-center"
+              >
+                <div className="flex flex-col text-center gap-3">
+                  <Image
+                    width={500}
+                    height={500}
+                    src="https://picsum.photos/500/500"
+                    alt={product.name}
+                    className="object-contain"
+                  />
+                  <p className="px-5 line-clamp-2">{product.name}</p>
+                  <p className="font-bold">{formatThaiBaht(product.price)}</p>
+                  <ProductRating rating={productRating} />
+                  <Link
+                    href={`products/${product.id}`}
+                    className="bg-theme-color px-5 py-3 rounded text-white mx-3"
+                  >
+                    เรียนรู้เพิ่มเติม
+                  </Link>
+                  <Link href="#" title="" className="py-4 text-theme-color">
+                    <FontAwesomeIcon icon={faLocationDot} className="pe-1" />
+                    <span>สถานที่จัดจำหน่าย</span>
+                  </Link>
                 </div>
-                <Link
-                  href={`products/${product.id}`}
-                  className="bg-theme-color px-5 py-3 rounded text-white mx-3"
-                >
-                  เรียนรู้เพิ่มเติม
-                </Link>
-                <Link href="#" title="" className="py-4 text-theme-color">
-                  <FontAwesomeIcon icon={faLocationDot} className="pe-1" />
-                  <span>สถานที่จัดจำหน่าย</span>
-                </Link>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
