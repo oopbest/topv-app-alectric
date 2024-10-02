460b2910a735dcaa9553ae5100b4b34a9544795b
Setthawut Pimthong <oop_best@yahoo.com>
2024-10-02 11:52:45 +0700

    update warranty page

--- src/app/warranty/page.tsx
+++ src/app/warranty/page.tsx
@@ -3,7 +3,7 @@ const WanrrantyPage = () => {
     <>
       <section className="flex mx-auto bg-theme-container">
         <div className="container mx-auto p-4">
-          <div className="relative w-full h-0 pb-[90.25%] sm:pb-[50.25%]">
+          <div className="w-full h-screen md:h-screen lg:h-[calc(100vh-4rem)] relative">
             <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLSdROtsWJaCUNh21RAJMe02VL7PI9cTW8CL9wV-JafC3tO30PQ/viewform?embedded=true"
               className="absolute top-0 left-0 w-full h-full border-0"
