c163ef2dab7e90609f0004da43617dc2e99dc790
Setthawut Pimthong <oop_best@yahoo.com>
2024-10-02 11:41:55 +0700

    update warranty page

--- src/app/warranty/page.tsx
+++ src/app/warranty/page.tsx
@@ -2,7 +2,16 @@ const WanrrantyPage = () => {
   return (
     <>
       <section className="flex mx-auto bg-theme-container">
-        <div className="container mx-auto p-4">Warranty Page</div>;
+        <div className="container mx-auto p-4">
+          <div className="relative w-full h-0 pb-[90.25%] sm:pb-[50.25%]">
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdROtsWJaCUNh21RAJMe02VL7PI9cTW8CL9wV-JafC3tO30PQ/viewform?embedded=true"
+              className="absolute top-0 left-0 w-full h-full border-0"
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+        ;
       </section>
     </>
   );
